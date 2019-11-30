9cfedee9c98749823f675e7151b1175b0851e6b3
yourjia <2086543608@qq.com>
2019-11-30 22:52:43 +0800

    20191130 观察多线程

--- src/MyThread.java
+++ src/MyThread.java
@@ -620,6 +620,7 @@ thread3.start();
 }
 */
 
+/*
 //同步方法
 public class MyThread implements Runnable
 {
@@ -661,8 +662,54 @@ System.out.println(Thread.currentThread().getName()+",还有"+ticket--+"张票")
         thread2.start();
         thread3.start();
         myThread.print();
-
-
     }
     }
+*/
+
+
+
+//观察synchronized锁多对象
+class Sync
+{public synchronized void test()
+{
+    System.out.println("test方法开始，当前线程为" + Thread.currentThread().getName());
+
+    try {
+        Thread.sleep(1000);
+    } catch (InterruptedException e) {
+        e.printStackTrace();
+    }
+    System.out.println("test方法结束，当前线程为" + Thread.currentThread().getName());
+}}
+
+public class MyThread extends Thread
+{
+    public void run()
+{
+    Sync sync=new Sync();
+    sync.test();
+}
+    public static void main(String[] args) {
+        for(int i=0;i<3;i++)
+        {
+            Thread thread=new MyThread();
+            thread.start();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
