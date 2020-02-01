104a247f9bc3521cc9e80fe19a0b1b8bdd4785ef
yourjia <2086543608@qq.com>
2020-02-01 18:37:22 +0800

    20200201 LockSupport

--- src/LockSupport/Test1.java
+++ src/LockSupport/Test1.java
@@ -1,4 +1,24 @@
 package LockSupport;
 
+//使用LockSupport
+
+import java.util.concurrent.locks.LockSupport;
+
 public class Test1 {
+    public static void main(String[] args) {
+        Thread thread=new Thread(()->
+        {
+            LockSupport.park();
+            System.out.println(Thread.currentThread().getName()+"被唤醒");
+        });
+        thread.start();
+        try
+        {
+            Thread.sleep(2000);
+        }
+        catch (InterruptedException e)
+        {e.printStackTrace();}
+        LockSupport.unpark(thread);
+    }
+
 }
